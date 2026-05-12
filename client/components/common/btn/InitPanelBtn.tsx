70d90d020d2360019928b9662e55fddae35c2c47
leesuyeon <lgh0385hh@gmail.com>
2026-05-12 19:50:51 +0900

    feat: Init Panel 생성

---
+++ client/components/common/btn/InitPanelBtn.tsx
@@ -0,0 +1,8 @@
+
+export default function InitPanelBtn() {
+    return (
+        <div className='text-purple-950 font-black bg-pink px-10 py-2'>
+            <span>Initialize Session</span>
+        </div>
+    )
+}
--- client/components/index.ts
+++ client/components/index.ts
@@ -1,3 +1,7 @@
+// Panel Components
+export { default as MainPanel } from "./common/panel/MainPanel";
+
+
 // SideBar Components
 export { default as SideBarTabList } from "./sidebar/SideBarTabList";
 export { default as ActiveAgentsList } from "./sidebar/ActiveAgentsList";
@@ -5,9 +9,14 @@ export { default as ActiveAgentsList } from "./sidebar/ActiveAgentsList";
 // Button Components
 export { default as PrimaryButton } from "./common/btn/PrimaryButton";
 export { default as StepButton } from "./common/btn/StepButton";
+export { default as InitPanelBtn } from "./common/btn/InitPanelBtn";
+
 
 // modal Deploy Components
 export { default as StepTemplate } from "./modal/deploy/StepTemplate";
 export { default as StepFirstTemplate } from "./modal/deploy/StepFirstTemplate";
 export { default as StepSecondTemplate } from "./modal/deploy/StepSecondTemplate";
 export { default as StepThirdTemplate } from "./modal/deploy/StepThirdTemplate";
+
+// Icon Components
+export { default as InitPanelIcon } from "./common/icon/InitPanelIcon";
