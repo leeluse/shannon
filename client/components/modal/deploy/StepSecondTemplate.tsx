5a8ae0c44197df6b7aa0f6910d042190b92059d5
leesuyeon <lgh0385hh@gmail.com>
2026-05-31 21:28:57 +0900

    feat: implement agent configuration form and model selection list in deploy step

--- client/components/modal/deploy/StepSecondTemplate.tsx
+++ client/components/modal/deploy/StepSecondTemplate.tsx
@@ -1,7 +1,45 @@
+import { MODEL_LIST } from '@/mock/modal'
 import React from 'react'
+import ModelCard from '../card/ModelCard'
 
 export function StepSecondTemplate() {
     return (
-        <div>StepSecondTemplate</div>
+        <div className='grid grid-cols-2 size-full'>
+            <TemplateCardDetail />
+            <ModelSelector />
+        </div>
+    )
+}
+
+
+export function TemplateCardDetail() {
+    return (
+        <div>
+            <h1 className='text-sm font-medium text-c-secondary-sub/50'>에이전트</h1>
+            <div className=''>
+                <div className='flex flex-col gap-1 py-2'>
+                    <span className='text-xs'>이름</span>
+                    <input className='bg-amber-50/5 rounded-sm focus:outline-none py-1 px-2' type="text" />
+                </div>
+                <div className='flex flex-col gap-1 py-2'>
+                    <span className='text-xs'>에이전트 상세 설명</span>
+                    <input className='bg-amber-50/5 rounded-sm focus:outline-none py-1 px-2' type="text" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export function ModelSelector() {
+    return (
+        <div>
+            <h1 className='text-sm font-medium text-c-secondary-sub/50'>모델</h1>
+            {MODEL_LIST.map(({ id, name }) => (
+                <div key={id}>
+                    <ModelCard name={name} />
+                </div>
+            ))}
+        </div>
     )
 }
