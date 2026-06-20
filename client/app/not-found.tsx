438ae0794dc44c2587c473df8a9ede9317a316d7
Suyeon Lee <122202949+leeluse@users.noreply.github.com>
2026-06-20 20:17:48 +0900

    feat: 페이지 라우팅 flow 설계 (#29)
    * feat: Dashboard Redirect 추가
    
    * feat: 잘못된 경로  fallback 처리

---
+++ client/app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFoundPage() {
+    return (
+        <div className='flex flex-col gap-4 items-center justify-center'>
+            <span className='text-lg font-bold'>404 - Page Not Found</span>
+        </div>
+    )
+}
--- client/components/common/Logo.tsx
+++ client/components/common/Logo.tsx
@@ -1,8 +1,39 @@
+"use client"
 import Image from 'next/image'
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 export default function Logo() {
+    const router = useRouter();
+    const onClickHandler = () => {
+        router.push('/dashboard')
+    }
+
     return (
-        <Image src="/logo.png" alt="Shannon Logo" width={48} height={48} className="size-12 rounded-xl" />
+        <>
+            <div className="shrink-0">
+                <LogoImg onClick={onClickHandler} />
+            </div>
+            <div className="flex flex-col gap-0 min-w-0">
+                <button
+                    onClick={onClickHandler}
+                    className='text-2xl text-mauve-600 truncate'>Shannon</button>
+                <span className='text-sm font-medium text-mauve-300 truncate'>v0.0.1</span>
+            </div>
+        </>
+    )
+}
+
+
+export function LogoImg({ onClick }: {
+    onClick?: () => void
+}) {
+    return (
+        <Image
+            src="/logo.png"
+            alt="Shannon Logo"
+            width={48} height={48}
+            onClick={onClick}
+            className="size-12 rounded-xl cursor-pointer" />
     )
 }
